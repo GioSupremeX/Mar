7c953e6160057088968f8fdfe6bbe1f523d41bb0
GioSupremeX <charalgiorgos2011@gmail.com>
2026-08-09 02:21:22 +0300

    Update app.ts

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -1,7 +1,8 @@
 import express, { type Express } from "express";
 import cors from "cors";
 import pinoHttp from "pino-http";
-import path from "path"; // <-- 1. ADD THIS IMPORT
+import fs from "fs";
+import path from "path";
 import router from "./routes";
 import { logger } from "./lib/logger";
 
